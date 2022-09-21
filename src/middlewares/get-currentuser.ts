4c519fef23f845fc3d40c15afba3f4ed8fd218c0
umerkang66 <umerkang66@gmail.com>
2022-09-21 21:05:43 +0500

    updates

--- src/middlewares/get-currentuser.ts
+++ src/middlewares/get-currentuser.ts
@@ -4,11 +4,14 @@ import { NextFunction, Request, Response } from 'express';
 
 type Role = 'user' | 'writer' | 'admin';
 // interface that describes the properties that user document has
-export interface UserDocument extends mongoose.Document {
+interface UserDocument extends mongoose.Document {
   email: string;
   password: string;
   role: Role;
+  changedPasswordAfter: (jwtIssuedAt: number) => boolean;
 }
+
+interface UserModelInterface extends mongoose.Model<UserDocument> {}
 
 declare global {
   namespace Express {
@@ -18,7 +21,7 @@ declare global {
   }
 }
 
-export function getCurrentuser(UserModel: mongoose.Model<UserDocument>) {
+export function getCurrentuser(UserModel: UserModelInterface) {
   return async (req: Request, res: Response, next: NextFunction) => {
     // this req.cookies and req.cookies.jwt is coming from cookie-parser library
     if (!req.cookies || !req.cookies.jwt) {
@@ -34,6 +37,13 @@ export function getCurrentuser(UserModel: mongoose.Model<UserDocument>) {
       // payload.id is set in the createSendToken
       const user = await UserModel.findById(payload.id);
       if (!user) return next();
+
+      // if the user's password has been changed after the token has been issued
+      // then token should not work
+      if (user.changedPasswordAfter(payload.iat as number)) {
+        return next();
+      }
+
       req.currentuser = user;
       // if there is error don't do anything, there could be an error, if jwt_key is not correct
     } catch (err) {}
