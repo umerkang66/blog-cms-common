67a1d384969c101b701999348cfd8334c96625f7
umerkang66 <umerkang66@gmail.com>
2022-09-23 22:14:39 +0500

    updates

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 // ERRORS
 export * from './errors/bad-request-error';
+export * from './errors/internal-server-error';
 export * from './errors/custom-error';
 export * from './errors/database-connection-error';
 export * from './errors/not-authorized-error';
